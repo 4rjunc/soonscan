806359626aab8d5eeeec31fbbddc63403c2bae88
4rjunc <arjunmundamani@gmail.com>
2024-11-23 21:10:15 +0530

    modulize

--- src/app.rs
+++ src/app.rs
@@ -16,8 +16,6 @@ use ratatui::{
     DefaultTerminal, Frame,
 };
 
-mod app;
-
 // This will store the state of the application
 #[derive(Debug, Default)]
 pub struct App {
@@ -186,29 +184,6 @@ impl Widget for &App {
 
         table.render(area, buf);
     }
-}
-
-#[tokio::main]
-async fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-
-    // Take command-line argument for initial query (transaction hash)
-    let initial_query = if args.len() > 1 {
-        args[1].clone() // Use the provided argument (transaction hash)
-    } else {
-        eprintln!("Error: Transaction hash is required as a command-line argument.");
-        std::process::exit(1);
-    };
-
-    let mut terminal = ratatui::init();
-    let mut app = App {
-        query: initial_query,
-        ..App::default()
-    };
-
-    let app_result = app.run(&mut terminal).await;
-    ratatui::restore();
-    app_result
 }
 
 
